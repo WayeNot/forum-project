5cb783a3e9e788bc20cb3af5b8e5d52211d43709
JeremieAVoid <jeremieadam44@proton.me>
2026-06-03 09:03:10 +0200

    [MERGE] fix

--- internal/db/migrations/20260601074957_update_tag_post_table.sql
+++ internal/db/migrations/20260601074957_update_tag_post_table.sql
@@ -1,7 +1,6 @@
 -- +goose Up
 ALTER TABLE posts DROP COLUMN tags;
-ALTER TABLE posts ADD COLUMN tags TEXT DEFAULT '';
+ALTER TABLE posts ADD COLUMN tags TEXT[] DEFAULT '{}';
 
 -- +goose Down
-ALTER TABLE posts DROP COLUMN tags;
-ALTER TABLE posts ADD COLUMN tags TEXT DEFAULT '';
+ALTER TABLE posts ALTER COLUMN tags TYPE TEXT USING array_to_string(tags, ',') DEFAULT '';
